63efef1bf376fb6c251bddfa624587e3f42a19e2
bacter00 <h3po4@protonmail.com>
2022-05-30 14:03:14 +0200

    Add cd js- articles

--- js/script.js
+++ js/script.js
@@ -5,9 +5,25 @@ function titleClickHandler(event){
 
 /* remove class 'active' from all article links  */
 
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+
   /* add class 'active' to the clicked link */
 
+
   /* remove class 'active' from all articles */
+
+  const activArticles = document.querySelectorAll('.articles a.active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  } 
+
+
 
   /* get 'href' attribute from the clicked link */
 
