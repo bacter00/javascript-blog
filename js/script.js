beee4873092083c0f4d43ec2150e7641fd2bf978
bacter00 <h3po4@protonmail.com>
2022-06-13 16:13:26 +0200

    Add 54 wstepnie

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@ function titleClickHandler(event){
 
   clickedElement.classList.add('active');
   
-
   console.log('clickedElement:', clickedElement);
 
 
@@ -33,45 +32,98 @@ function titleClickHandler(event){
   }
 
 
-
   /* get 'href' attribute from the clicked link */
-
-
 
 const articleSelector = 'href' ;
 
- let href = clickedElement.getAttribute('href')
+ let href = clickedElement.getAttribute(articleSelector)
 
- console.log (href) ;
-
-
+ console.log (href);
 
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
-  const targetArticle = document.querySelector('');
+  const targetArticle = document.querySelector(href);
 
+  console.log (targetArticle) ;
   
 
  
-  
-  
-
-
-
-
   /* add class 'active' to the correct article */
 
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.add('active');
-  }
-
-
-
+  
+    targetArticle.classList.add('active');
+  
 
 }
+
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+
+
+
+
+
+
+const optArticleSelector = '.post',
+      optTitleSelector = '.post-title',
+      optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+
+  /* remove contents of titleList */
+
+const titleList = document.querySelector(optTitleListSelector).innerHTML='' ;
+ 
+
+
+
+
+
+
+  /* for each article  - brakuje petli*/
+
+  const articles = document.querySelectorAll('.post') ;
+
+console.log (articles);
+
+  
+
+
+    /* get the article id   - znowu cos zle*/
+
+    const articleId = 'id'
+    let id = '1'
+
+console.log = (articleID);
+
+
+
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  
+  
+  
+  
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+ 
+    console.log (linkHTML)
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
